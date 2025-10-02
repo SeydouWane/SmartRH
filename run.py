4d6755f21e3dbffcd1206295bfb42ac878cabeb5
Papa Seydou WANE <129470066+SeydouWane@users.noreply.github.com>
2025-10-02 21:22:35 +0000

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Récupère le port de la variable d'environnement 'PORT', ou utilise 5000 par défaut.
+    # Définit l'hôte sur '0.0.0.0' pour l'accès externe.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
